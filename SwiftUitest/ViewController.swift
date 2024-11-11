e11eb963519072769ad9bc7f763f3c35dbd643b1
SShrey1 <palishrey@gmail.com>
2024-11-11 13:03:45 +0530

    Initial Commit

---
+++ SwiftUitest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUitest
+//
+//  Created by user@59 on 20/08/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
